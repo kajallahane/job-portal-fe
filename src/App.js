ec96e50839381275088e60934ac3366858be95c9
kajallahane <kajallahane1993@gmail.com>
2020-09-05 17:23:49 +0530

    Add candidate page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from "./pages/home/Home";
+import JobList from './pages/candidate/job-list'
+import PersistentDrawerLeft from './pages/assests/drawer'
 
 class App extends React.Component {
     /*rendering view*/
@@ -9,7 +11,9 @@ class App extends React.Component {
         return (
             <Router>
                 <div className="">
-                    <Route path="/" render={(props) => <Home {...props} />} />
+                    <Route  exact={true} path="/candidatehome" render={(props) => <JobList {...props} />} />
+                    <Route  exact={true} path="/PersistentDrawerLeft" render={(props) => <PersistentDrawerLeft {...props} />} />
+                    <Route exact={true} path="/" render={(props) => <Home {...props} />} />
                 </div>
             </Router>
         );
---
+++ src/pages/candidate/job-list.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import OutlinedCard from '../assests/cards'
+import PersistentDrawerLeft from '../assests/drawer'
+
+export default function JobList(){
+    
+        let postedJob = [
+            {jobId: 1, title:"Senior Software Enginner", location:"Pune", organization: "TCS", experience:3},
+            {jobId: 2, title:"Feature Team Lead", location:"Australia", organization: "Telstra",experience:5}
+        ]
+        const routeData = [
+            {url:"", item:"Job"},
+            {url:"", item:"Request Status"}
+        ]
+        
+        return (    
+            <div className="job-list">
+                <PersistentDrawerLeft routeData={routeData} />
+                    <div id="cardsDiv" style={{"margin": "auto","width": "65%"}}>
+                    {
+                        postedJob.map( obj=> {
+                            return OutlinedCard(obj) 
+                        })
+                    }
+                    </div>
+            </div>
+        );
+    
+}
