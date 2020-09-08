253562e4fe8ac92cad273a3ebd137f0c9fa4d66a
kajallahane <kajallahane1993@gmail.com>
2020-09-08 22:25:35 +0530

    Point app to heroku

--- src/server/EndPoint.js
+++ src/server/EndPoint.js
@@ -1,11 +1,11 @@
 const EndPoint = {
+  APP_BASE_URL: 'https://job-portal-kajal-be.herokuapp.com',
+  // APP_BASE_URL: 'http://localhost:8085', // local env
   CANDIDATE_JOBLIST: "/candidate/joblist",
-  RECRUITER_REQUEST_LIST:"/recruiter/jobrequests/567",
-  //APP_BASE_URL: 'https://job-portal-kajal-be.herokuapp.com',
-  APP_BASE_URL: 'http://localhost:8085', // local env
+  RECRUITER_REQUEST_LIST: "/recruiter/jobrequests/567",
   CANDIDATE_JOB_REQUEST_LIST: '/candidate/jobrequests',
-  CANDIDATE_INFO :"/candidate/info/",
-  ADD_JOB_REQUEST:"/candidate/insertjobrequest",
-  UPDATE_JOB_REQUEST_STATUS:"/recruiter/updatejobrequests"
+  CANDIDATE_INFO: "/candidate/info/",
+  ADD_JOB_REQUEST: "/candidate/insertjobrequest",
+  UPDATE_JOB_REQUEST_STATUS: "/recruiter/updatejobrequests"
 };
 export default EndPoint;
